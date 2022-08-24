bf4471c69d3d0c295175997a62571c886a675dc6
RATHISHKUMAR07t <rathishkumar.ct19@bitsathy.ac.in>
2022-08-24 19:38:55 +0530

    JS Set to RegX

--- javascript.js
+++ javascript.js
@@ -74,7 +74,89 @@ Object-Date-Array-String-Number-Boolean
 And 2 data types that cannot contain values:
 
 null-undefined
+--------------------JS TYPE CONVERSION------------------
+
+document.write(Number("3.14") + "<br>");
+document.write(Number(Math.PI) + "<br>");
+document.write(Number("   ") + "<br>");
+document.write(Number("") + "<br>");
+document.write(Number("99.88") + "<br>");
+document.write(Number("John") + "<br>");
+
+document.write(String(123) + "<br>");
+document.write(String(100 + 23) + "<br>");
+
+---------------------JS REGULAR EXP---------------------
+syntax - /pattern/modifiers;
+
+//STRING SEARCH
+
+let txt = "Visit W3";
+let pos = txt.search("W3");
+document.write(pos);
+
+//Using RegX
+let text = "Visit W3";
+let post = text.search(/W3/i);
+document.write(post);
+
+//STRING REPLACE
+let txt = "Hello Everyone";
+let repl = txt.replace("Everyone", "!");
+document.write(repl);
+
+//Using RegX
+let replace = txt.replace(/Everyone/i, "!");
+document.write(replace);
+
+Modifier
+
+i ---> Perform case-insensitive match {eg: "w3" -- "W3"} ==>W3
+g ---> Perform global match(finds all matchings)
+        {eg: "I hello I am",I} ==>I I
+m ---> Perform multiline match
+
+let text = "Hello hi Hello hi";
+let result = text.match(/Hello/g);
+document.write(result);
+
+let txt = "\nIs th\nis it?";
+let res = txt.match(/^is/m);
+document.write(res);
+
+[abc] ---> Find any characters btw brackets
+[0-9] ---> Find any digits btw brackets
+(x|y) --->Find any of the alternatives separated with |
+
+let txt = "Hello I am rathish kumar 1234";
+let res = txt.match(/[la]/g);
+document.write(res);
+
+let res1 = txt.match(/[1-3]/g);
+document.write(res1);
+
+let txt = "Give 100% level";
+//  \d --> find a digit
+let res1 = txt.match(/\d/g);
+//  \s --> find a whitespace character
+let res2 = txt.match(/\s/g);
+//  \b --> find a match at beg or end
+let res3 = txt.search(/\ble/g); //Beginning
+let res4 = txt.search(/el\b/g);
+
+document.write(res1 + " " + res2 + " " + res3 + " " + res4);
+
+//  o+ --> Matches any string at least one n
+//  o* --> Matches any string that contains 0 or more occur of o
+
+let txt = "Helloo world";
+let res = txt.match(/o+/g);
+document.write(res);
+
+
+const pattern = /a/;
+//Since there is an "e" in the string, the output will true
+document.write(pattern.test("Rathish Kumar"));
+document.write(/t/.test("Rathish Kumar"));
 
 */
-
-
